5072834fff754e3ee4b43606a895c0dfe24f632d
jswini <jswinikoff@gmail.com>
2021-10-27 15:53:16 -0500

    console commands for quit, eof, and emptyline

--- console.py
+++ console.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""
+hbnb terminal app
+"""
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """
+    methods for the basic controls of the hbnb terminal app
+    """
+    prompt = '(hbnb)'
+
+    def do_quit(self, *args):
+        """
+        use quit to exit the terminal
+        """
+        return True
+
+    def do_EOF(self, *args):
+        """
+        use ctrl+d (EOF) to exit the terminal
+        """
+        return True
+
+    def emptyline(self):
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
