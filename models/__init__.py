bd6d6f81bdad3ab50e8234cf3f0129c266fc81a1
ronroeandassociates <3321@holbertonschool.com>
2021-10-27 07:52:27 -0700

    class FileStorage that serializes instances to a JSON file and deserializes JSON file to instances

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""
+"""
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+
+
+available_models = {'BaseModel' : BaseModel}
+storage = FileStorage()
+storage.reload()
