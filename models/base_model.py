6409423ca3e8e2a1fce293d8bda3b2782845f33c
jswini <jswinikoff@gmail.com>
2021-10-29 11:58:26 -0500

    added parse time to init

--- models/base_model.py
+++ models/base_model.py
@@ -20,6 +20,9 @@ class BaseModel:
             for key, value in kwargs.items():
                 if "__class__" not in key:
                     setattr(self, key, value)
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key,
+                            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
