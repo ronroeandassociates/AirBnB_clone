656534ad2ee07da62e04155366b74e0a52f686c5
ronroeandassociates <3321@holbertonschool.com>
2021-10-29 13:23:11 -0700

    testing user

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+Test Module: Ameinity
+test_base_model.py - unittests for "Amenity'
+"""
+import unittest
+from models.amenity import Amenity
+
+
+class test_amenity(unittest.TestCase)
+    """
+    Amenity Tests
+    """
+    def test_amenity(self):
+        """
+        does it exist
+        """
+        inst_1 = Amenity()
+        self.assertTrue(inst_1)
+
+    def test_amenity_instance_del(self):
+        """
+        test delete 
+        """
+        inst_1_1 = Amenity()
+        del inst_1_1
+
+    def test_amenity_instance(self)
+        """
+        test instance
+        """
+        inst_2 = Amenity()
+        self.asertisInstance(inst_2, Amenity)
+
+    def test_amenity_save(self):
+        """
+        test save
+        """
+        updated_amenity = inst_3.updated_at
+        inst_3.save = Amenity()
+        new_inst_3 = inst_3.updated_at
+        self.assertNotEqual(updated_amenity, new_inst_3)
+
+    def test_name_str(self):
+        """
+        test type 
+        """
+        inst_4 = Amenity()
+        self.assertIsInstance(inst_4.name, str)
+    
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,1 +1,76 @@
 #!/usr/bin/python3
+"""
+Test Module: User
+test_base_model.py - unittests for 'User'
+"""
+import unittest
+from models.user import User
+
+
+class test_user(unittest.TestCase):
+    """
+    User Tests
+    """
+    def test_user(self):
+        """
+        does user exists
+        """
+        inst_1 = User()
+        self.assertTrue(inst_1)
+
+    def test_user_instance_del(self):
+        """
+        can be deleted 
+        """
+        inst_1_1 = User()
+        del inst_1_1
+
+    def test_user_instance(self):
+        """
+        test user instance
+        """
+        inst_2 = User()
+        self.assertIsInstance(inst_2, User)
+
+    def test_user_save(self):
+        """
+        test if can save
+        """
+        inst_3 = User()
+        updated_user = inst_3.updated_at
+        inst_3.save()
+        new_inst_3 = inst_3.updated_at
+        self.assertNotEqual(updated_user, new_inst_3)
+
+    def test_email_str(self):
+        """
+        test user type
+        """
+        inst_4 = User()
+        self.assertIsInstance(inst_4.email, str)
+
+    def test_password_str(self):
+        """
+        test user type
+        """
+        inst_5 = User()
+        self.assertIsInstance(inst_5.password, str)
+
+    def test_first_name_str(self):
+        """
+        test user type
+        """
+        inst_6 = User()
+        self.assertIsInstance(inst_6.first_name, str)
+
+    def test_last_mane_str(self):
+        """
+        test user type
+        """
+        inst_7 = User()
+        self.assertIsInstance(inst_7.last_name, str)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
