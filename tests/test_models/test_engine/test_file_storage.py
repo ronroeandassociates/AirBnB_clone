68b94716eb95d85afc5c72b5dfbdecfa61f1d9b3
ronroeandassociates <3321@holbertonschool.com>
2021-10-29 11:25:06 -0700

    adding tests

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+file test strage
+file storage test
+"""
+import unittest
+import uuid
+from models.base_model import BaseModel
+from models.engine.filestorage import BaseModel
+from models.engine.file_storage import FileStorage
+
+
+class test_file_storage(unittest.TestCase):
+    """
+    test file storage
+    """
+    def setUp(self):
+        """ setup test for FileStorage
+        """
+        self.storage = FileStorage()
+
+    def test_attrs(self):
+        """ 
+        test for attributes
+        """
+        self.assertFalse(hasattr(self.storage, "mw.json"))
+
+    if "__main__" == __name__:
+        unittest.main()
+
